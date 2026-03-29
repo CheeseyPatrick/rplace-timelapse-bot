01af0989e45df0834c52973a9ee22cec95448687
CheeseyPatrick <patrickfunmcginley@gmail.com>
2026-03-28 20:03:54 -0400

    Proxy support

--- game-worker-wrapper.js
+++ game-worker-wrapper.js
@@ -1,5 +1,6 @@
 import { parentPort } from "worker_threads";
 import { WebSocket } from "ws";
+import { SocksProxyAgent } from "socks-proxy-agent";
 
 // We need to patch some things to make our node environment act like the normal browser environment that the worker would normally be in
 
@@ -10,11 +11,17 @@ Buffer.prototype.arrayBuffer = function () {
   );
 };
 
+// You can use a proxy if you set the SOCKS5_PROXY environment variable
+const agent = process.env.SOCKS5_PROXY
+  ? new SocksProxyAgent(process.env.SOCKS5_PROXY)
+  : undefined;
+
 // Polyfill browser WebSocket with required headers
 const OriginalWebSocket = WebSocket;
 globalThis.WebSocket = class extends OriginalWebSocket {
   constructor(url, protocols) {
     super(url, protocols, {
+      agent,
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
